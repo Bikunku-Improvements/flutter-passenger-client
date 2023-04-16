7e8abd4b24d48b66221b02d34d20912da6c10b52
Mario Serano <marioserano55@gmail.com>
2023-04-17 01:13:04 +0700

    added demo to have end-to-end request to ui

--- lib/homepage/presentation/terminal_view.dart
+++ lib/homepage/presentation/terminal_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:passenger_client/homepage/blocs/terminal_cubit.dart';
 import 'package:passenger_client/homepage/blocs/terminal_state.dart';
+import 'package:passenger_client/homepage/presentation/widgets/TerminalItem.dart';
 
 class TerminalView extends StatefulWidget {
   const TerminalView({super.key});
@@ -29,7 +30,15 @@ class _TerminalViewState extends State<TerminalView> {
           if (state.isLoadingTerminal) {
             return const Center(child: CircularProgressIndicator());
           }
-          return Center(child: Text(state.toString()));
+
+          return Container(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+            child: ListView(
+              children: state.terminalList
+                  .map((terminal) => TerminalItem(terminal: terminal))
+                  .toList(),
+            ),
+          );
         }));
   }
 }
