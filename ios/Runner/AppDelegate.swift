8dbdd52f6ae8826c89ab509accac5f46e441de89
Mario Serano <marioserano55@gmail.com>
2023-04-19 20:10:54 +0700

    added configuration for gmaps migration

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,7 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    GMSServices.provideAPIKey("AIzaSyDwsrdg0JBptnRpWs8O465B1jVCsHGtZF8")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
